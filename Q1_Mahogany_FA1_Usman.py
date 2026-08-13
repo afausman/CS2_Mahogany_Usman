813402600f4d09b570f4083c137297b28161b69c
afausman <afausman31@src.pshs.edu.ph>
2026-08-13 09:22:18 +0800

    Add files via upload

---
+++ Q1_Mahogany_FA1_Usman.py
@@ -0,0 +1,16 @@
+import math
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
+distance = math.sqrt(math.pow(x2 - x1, 2) + (math.pow(y2 - y1, 2)))
+print("The distance between the two points is: ", distance)
+
+"""
+Using the library made it more practical as it provides built-in functions for mathematical operations, making the code more efficient 
+to manipulate and run through. The built-in functions sqrt and pow were easier to use
+as without them, I would have to manually carry out the calculations, which would have made the program longer and more complex.
+The program would be more difficult without the functions sqrt() and pow() as manually, it would have made it more time-consuming
+and prone to errors.
+"""
