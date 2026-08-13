aded1e00e7bb3a64adfb16ddf8c4764df05ac4b7
afausman <afausman31@src.pshs.edu.ph>
2026-08-13 09:49:37 +0800

    Improved FA1

--- Q1_Mahogany_FA1_Usman.py
+++ Q1_Mahogany_FA1_Usman.py
@@ -16,5 +16,5 @@ Using the library made it more practical as it provides built-in functions for m
 to manipulate and run through. The built-in functions sqrt and pow were easier to use
 as without them, I would have to manually carry out the calculations, which would have made the program longer and more complex.
 The program would be more difficult without the functions sqrt() and pow() as manually, it would have made it more time-consuming
-and prone to errors.
+and prone to errors. 
 """
